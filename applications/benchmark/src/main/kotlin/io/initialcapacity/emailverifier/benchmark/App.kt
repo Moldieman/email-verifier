b5823f8217c070755d83d25265665f51283f37fe
Alex Molde <alex@apphouse.co.za>
2023-11-08 15:50:08 +0200

    k6 performance tests

--- applications/benchmark/src/main/kotlin/io/initialcapacity/emailverifier/benchmark/App.kt
+++ applications/benchmark/src/main/kotlin/io/initialcapacity/emailverifier/benchmark/App.kt
@@ -19,8 +19,8 @@ fun main(): Unit = runBlocking {
     val benchmark = Benchmark(
         registrationUrl = System.getenv("REGISTRATION_URL") ?: "http://localhost:8081",
         registrationCount = getEnvInt("REGISTRATION_COUNT", 5_000),
-        requestWorkerCount = getEnvInt("REQUEST_WORKER_COUNT", 4),
-        registrationWorkerCount = getEnvInt("REGISTRATION_WORKER_COUNT", 4),
+        requestWorkerCount = getEnvInt("REQUEST_WORKER_COUNT", 8),
+        registrationWorkerCount = getEnvInt("REGISTRATION_WORKER_COUNT", 6),
         client = HttpClient(Java) {
             expectSuccess = false
         }
